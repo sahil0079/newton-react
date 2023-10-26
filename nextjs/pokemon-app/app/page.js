f134c52a61bf30a21fd8e6a3d43f6ad83590497f
sahil0079 <114945558+sahil0079@users.noreply.github.com>
2023-10-26 22:57:23 +0530

    Add files via upload

---
+++ nextjs/pokemon-app/app/page.js
@@ -0,0 +1,47 @@
+"use client"
+import Link from 'next/link';
+import { useEffect, useState } from 'react'
+import styles from './styles/Home.module.css';
+
+const URL = 'https://jherr-pokemon.s3.us-west-1.amazonaws.com/index.json'
+
+export default function Home() {
+
+  const [pokemon, setPokemon] = useState();
+
+
+  useEffect(() => {
+    async function getPokemons() {
+
+      const response = await fetch(URL);
+      const data = await response.json();
+      // setPokemon(await response.json());
+      setPokemon(data);
+    }
+
+    getPokemons();
+  }, [])
+
+  console.log(pokemon)
+
+  return (
+    <main className={styles.container}>
+      <h2>Pokemon List</h2>
+
+      <div className={styles.grid} >
+        {pokemon?.map((pokemon) => (
+          <div className={styles.card} key={pokemon.id}>
+
+            <Link href={`/pokemon/${pokemon.id}`} >
+              <img src={`https://jherr-pokemon.s3.us-west-1.amazonaws.com/${pokemon.image}`}
+                alt={pokemon.name} />
+              <h3>{pokemon.name}</h3>
+
+            </Link>
+
+          </div>
+        ))}
+      </div>
+    </main>
+  )
+}
---
+++ nextjs/pokemon-app/app/pokemon/[id]/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Details() {
+    return (
+        <div>Hello world</div>
+    )
+}
+
+export default Details
