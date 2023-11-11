69214488bce3d323c0ba608890425c1b46fdaa15
sahil0079 <114945558+sahil0079@users.noreply.github.com>
2023-11-11 09:02:20 +0530

    Add files via upload

---
+++ starterFiles/ui/AppLayout.jsx
@@ -0,0 +1,34 @@
+import { Outlet } from "react-router-dom";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+import styled from 'styled-components';
+
+
+
+const Main = styled.main`
+    background-color : var(--color-grey-50);
+    padding: 4rem 4.8rem 6.4rem
+    
+`;
+
+const StyledAppLayout = styled.div`
+    display: grid;
+    grid-template-columns: 26rem 1fr;
+    grid-template-rows: auto 1fr;
+    height: 100vh;
+`;
+
+
+function AppLayout() {
+	return (
+		<StyledAppLayout>
+			<Header />
+			<Sidebar />
+			<Main>
+				<Outlet />
+			</Main>
+		</StyledAppLayout>
+	);
+}
+
+export default AppLayout;
